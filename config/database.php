d02910c3c7bce0aca25a8572c5134e56ac116b4b
denyrachmat22 <denyrachmat22@gmail.com>
2026-07-30 08:29:26 +0700

    Add JAT MySQL database connection configuration

--- config/database.php
+++ config/database.php
@@ -63,6 +63,25 @@ return [
             ]) : [],
         ],
 
+        'jat_mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_JAT_HOST', '127.0.0.1'),
+            'port' => env('DB_JAT_PORT', '3306'),
+            'database' => env('DB_JAT_DATABASE', 'forge'),
+            'username' => env('DB_JAT_USERNAME', 'forge'),
+            'password' => env('DB_JAT_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
